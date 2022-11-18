25ff2988351d474d0ea02e0d414750b9888cd38f
unknown <ttb2k1@gmail.com>
2022-11-18 15:55:21 +0700

    update UI

---
+++ frontend/src/Components/Slides/data.js
@@ -0,0 +1,110 @@
+export const data = [
+  {
+    name: '気',
+    subname: 'KHÍ',
+  },
+  {
+    name: '日',
+    subname: 'NHẬT',
+  },
+  {
+    name: '百',
+    subname: 'BÁCH',
+  },
+  {
+    name: '子',
+    subname: 'TỬ',
+  },
+  {
+    name: '女',
+    subname: 'NỮ',
+  },
+  {
+    name: '今',
+    subname: 'KIM',
+  },
+  {
+    name: '読',
+    subname: 'ĐỘC',
+  },
+  {
+    name: '水',
+    subname: 'THỦY',
+  },
+  {
+    name: '天',
+    subname: 'THIÊN',
+  },
+  {
+    name: '校',
+    subname: 'GIÁO',
+  },
+  {
+    name: '父',
+    subname: 'PHỤ',
+  },
+  {
+    name: '見',
+    subname: 'KIẾN',
+  },
+  {
+    name: '木',
+    subname: 'MỘC',
+  },
+  {
+    name: '下',
+    subname: 'HẠ',
+  },
+  {
+    name: '行',
+    subname: 'HÀNH',
+  },
+  {
+    name: '学',
+    subname: 'HỌC',
+  },
+  {
+    name: '大',
+    subname: 'ĐẠI',
+  },
+  {
+    name: '聞',
+    subname: 'VĂN',
+  },
+  {
+    name: '小',
+    subname: 'TIỂU',
+  },
+  {
+    name: '左',
+    subname: 'TẢ',
+  },
+  {
+    name: '何',
+    subname: 'HÀ',
+  },
+  {
+    name: '北',
+    subname: 'BẮC',
+  },
+  {
+    name: '十',
+    subname: 'THẬP',
+  },
+  {
+    name: '二',
+    subname: 'NHỊ',
+  },
+  {
+    name: '山',
+    subname: 'SAN',
+  },
+  {
+    name: '入',
+    subname: 'NHẬP',
+  },
+  {
+    name: '時',
+    subname: 'THÌ',
+  },
+];
--- frontend/src/Components/Slides/index.jsx
+++ frontend/src/Components/Slides/index.jsx
@@ -1,11 +1,24 @@
 import styles from './Slide.module.scss'
-
-import React from 'react'
+import React, { useState } from 'react'
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 
 const Slide = () => {
+  const [slideIndex, setSlideIndex] = useState(0)
+  const handlerClick = (direction) => {
+    if (direction === 'up') {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <div className={styles.slideContainer}>
-        
+      <Arrow className={styles.Arrow} direction="up" onClick={() => handlerClick('up')}>
+        <KeyboardArrowUpOutlinedIcon />
+      </Arrow>
+      <Arrow direction="down" onClick={() => handlerClick('down')}>
+        <KeyboardArrowUpOutlinedIcon />
+      </Arrow>
     </div>
   )
 }
